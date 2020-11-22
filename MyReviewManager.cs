9bc35dc50c2ea1db9df0709cecaf34d457413d11
LifeIsJourney <44763493+LifeIsJourney@users.noreply.github.com>
2020-11-22 13:11:32 +0530

    In app review unity Youtube

---
+++ MyReviewManager.cs
@@ -0,0 +1,41 @@
+﻿using Google.Play.Review;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReviewManager : MonoBehaviour
+{
+    // Create instance of ReviewManager
+    private ReviewManager _reviewManager;
+     
+    public void ShowTheReview()
+    {
+        StartCoroutine(StartTheReview());
+    }
+
+    public IEnumerator StartTheReview()
+    {
+
+        _reviewManager = new ReviewManager();
+        var requestFlowOperation = _reviewManager.RequestReviewFlow();
+        yield return requestFlowOperation;
+        if (requestFlowOperation.Error != ReviewErrorCode.NoError)
+        {
+            // Log error. For example, using requestFlowOperation.Error.ToString().
+            yield break;
+        }
+
+        var _playReviewInfo = requestFlowOperation.GetResult();
+        var launchFlowOperation = _reviewManager.LaunchReviewFlow(_playReviewInfo);
+        yield return launchFlowOperation;
+        _playReviewInfo = null; // Reset the object
+        if (launchFlowOperation.Error != ReviewErrorCode.NoError)
+        {
+            // Log error. For example, using requestFlowOperation.Error.ToString().
+            yield break;
+        }
+        // The flow has finished. The API does not indicate whether the user
+        // reviewed or not, or even whether the review dialog was shown. Thus, no
+        // matter the result, we continue our app flow.
+    }
+}
